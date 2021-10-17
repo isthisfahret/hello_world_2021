6d580de40cdb3265136e76d24a74056bd4b8c76b
Fahret Fijuljanin <ffijuljani1@etf.unsa.ba>
2021-10-17 12:31:51 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+    public static void main (String[] args) {
+        
+        System.out.println("Does this look like a good day to you?");
+    }
+}
+
+
